c7dd6f0c1ed7d81570454fd02850c4d4f1c4a62a
Zac Reid <zacreid45@gmail.com>
2020-05-25 12:29:33 -0700

    Cleaned up some stuff

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-from flask import request, Flask
-import requests
 import re
 import os
-from slack import WebClient
-from slack.errors import SlackApiError
 
-token = "UYXuhkMMui7Vhk4APkQ7poaL"
-spotify_reg = "[^\/][\w]+(?=\?)"
+from flask import request, Flask
+import requests
+from slack import WebClient
+
 
 app = Flask(__name__)
 slack_token = os.environ["SLACK_API_TOKEN"]
+token = os.environ["SLACK_REQ_TOKEN"]
+
 client = WebClient(token=slack_token)
 slack_channel = "zar-test"
 
+spotify_reg = "[^\/][\w]+(?=\?)"
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
-@app.route('/new_message4', methods=['GET', 'POST'])
-def new_message4(): 
-    print("hello")
+@app.route('/add_to_playlist', methods=['GET', 'POST'])
+def add_to_playlist(): 
+    print("Adding song to playlist.")
     ret = ""
     try:
+        # This code path is required to validate an endpoint for slack events
         body = request.get_json()
         ret = body["challenge"]
     except:
-        print("no challenge")
+        print("No challenge token present.")
     try:
+        # 
         if body["token"] == token:
             print(str(body))
             url = body["event"]["links"][0]["url"]
-            print(url)
             spot_ids = re.findall(spotify_reg, url)
-            print(f"spotids: {spot_ids}")
             spot_id = ""
             if len(spot_ids) == 0:
                 spot_id = url.split("/track/")[-1]
@@ -43,9 +43,10 @@ def new_message4():
             print(spot_id)
 
             if ".com" not in spot_id:
+                print(f"Posting Spotify song: {spot_id} to {slack_channel}")
                 response = client.chat_postMessage(channel=slack_channel, text=spot_id)
     except :
-        print("no spotify song")
+        print("No spotify song found in the posted link.")
     return ret, 200
 
 if __name__ == '__main__':
