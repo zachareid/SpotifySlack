2ec1829bbc10106424a4dd954aa9f83a7783f972
Zac Reid <zacreid45@gmail.com>
2020-05-29 15:41:12 -0700

    set 1 gunicorn worker to try to fix threading issue

--- app.py
+++ app.py
@@ -206,4 +206,4 @@ def add_to_playlist():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded = False, port=5000)
+    app.run(port=5000)
